27db63f4556a59f019a8e3dc6834d68c92286481
wolfram77 <wolfram77@gmail.com>
2019-11-23 19:20:47 +0530

    llvm codegen for expression is now evailable

--- main.cpp
+++ main.cpp
@@ -2,14 +2,26 @@
 #include <stdio.h>
 
 using namespace std;
+using namespace llvm;
 extern Ast *yyroot;
 extern int yyparse();
+LLVMContext TheContext;
+IRBuilder<> Builder(TheContext);
+Module TheModule("BASIC", TheContext);
+map<string, Value*> NamedValues;
 
 
 int main(int argc, char **argv) {
   printf("> ");
   yyparse();
   printf("%s\n", yyroot->str().c_str());
+  FunctionType *FT = FunctionType::get(Type::getDoubleTy(TheContext), false);
+  Function *F = Function::Create(FT, Function::ExternalLinkage, "main", &TheModule);
+  BasicBlock *BB = BasicBlock::Create(TheContext, "entry", F);
+  Builder.SetInsertPoint(BB);
+  Builder.CreateRet(yyroot->code());
+  verifyFunction(*F);
+  TheModule.print(errs(), nullptr);
   return 0;
 }
 
